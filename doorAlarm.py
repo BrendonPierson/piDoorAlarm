c581913ee51962a8f8e1f8a29e4cef980c8c8b75
BrendonPierson <brendonpierson@gmail.com>
2015-07-25 18:22:50 -0500

    added subscriptions

--- doorAlarm.py
+++ doorAlarm.py
@@ -4,6 +4,25 @@ import RPi.GPIO as io
 import pygame
 import paho.mqtt.client as mqtt
 
+##### GPIO setup #####
+# set the pin numbering 
+io.setmode(io.BCM)
+
+# assign GPIO pins
+doorPin = 23
+alarmPin = 25
+disarmPin = 12
+# reed switch goes pin 23 to ground
+# piezo goes pin 25 to ground
+# disarm button goes pin12 to button to ground
+
+# set each pin as input or output, pull up resistors needed for switches
+io.setup(doorPin, io.IN, pull_up_down=io.PUD_UP)
+io.setup(alarmPin, io.OUT)
+io.setup(disarmPin, io.IN, pull_up_down=io.PUD_UP)
+io.output(alarmPin, 0)
+
+##### MQTT setup #####
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, rc):
     print("Connected with result code "+str(rc))
@@ -15,29 +34,23 @@ def on_connect(client, userdata, rc):
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
 
+#initialize client
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
 # Broker ip, Port, timeout
 client.connect("192.168.8.100", 1883, 60)
+
+#initial door closed status
 client.publish('backDoorStatus',payload='closed',qos=0,retain=False)
 
-io.setmode(io.BCM)
+#subscribe to alarm armed status from openhab
+subscribe([('alarmNoDelay', qos=0), ('alarmWDelay', qos=0)])
 
-doorPin = 23
-alarmPin = 25
-disarmPin = 12
-##reed switch goes pin 23 to ground
-##piezo goes pin 25 to ground
-##disarm button goes pin12 to button to ground
 
-io.setup(doorPin, io.IN, pull_up_down=io.PUD_UP)
-io.setup(alarmPin, io.OUT)
-io.setup(disarmPin, io.IN, pull_up_down=io.PUD_UP)
-
-io.output(alarmPin, 0)
-
+##### MUSIC setup #####
+# itialize the pygame music player for the alarm file
 pygame.mixer.init()
 pygame.mixer.music.load("siren.ogg")
 
@@ -49,6 +62,7 @@ def musicStop():
     pygame.mixer.music.stop()
     print("music stop")
 
+##### Main alarm function #####
 def alarm():
     print("door alarm")
     client.publish('backDoorStatus',payload='0',qos=0,retain=False)
@@ -56,14 +70,16 @@ def alarm():
         io.output(alarmPin, 1)
         musicStart()
         if (io.input(disarmPin) == False):
-            time.sleep(10)
+            #time.sleep(10) 
             io.output(alarmPin, 0)
             client.publish('backDoorStatus',payload='1',qos=0,retain=False)
             musicStop()
             break
 
+# Creates non-blocking loop for paho mqqt client to run
 client.loop_start()
 
+#try finally allows program to cleanup GPIO 
 try:
     while True:
         if io.input(doorPin):
